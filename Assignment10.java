bd6cc00995244c4db827f1d13cc547168a09d8bb
Yahya Iddris Fatah <123132207+balatwist@users.noreply.github.com>
2023-10-20 09:04:20 -0500

    update

---
+++ Assignment10.java
@@ -0,0 +1,28 @@
+public class Assignment10 {
+
+    //1. Pre-order traversal
+   // 50 10 8 5 9 30 26 31 150 100 79 126 152 151 180
+
+    //2. In-order traversal
+    //5 8 9 10 26 30 31 50 79 126 152 151 180
+
+    //3.Post-order traversal
+    //5 9 8 26 31 30 10 79 126 100 151 180 152 150 50
+
+    //4.inOrderTraversal(6);
+
+   /*
+   inOrderTraversal(null)
+   inOrderTraversal(10)->10
+   inOrderTraversal(null)
+   inOrderTraversal(6)->6
+   inOrderTraversal(9)->9
+   inOrderTraversal(null)
+   inOrderTraversal(4)->4
+    inOrderTraversal(null)
+    inOrderTraversal(1)->1
+     inOrderTraversal(3)->3
+   inOrderTraversal(6)->6
+   */
+
+}
